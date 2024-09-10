100430a47c89049af836e9974359ca98194e39b6
OvAlexander <alexanderleeov@gmail.com>
2024-09-10 14:37:41 -0700

    feat: created shield creation function

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from git import Repo
-
 username = "OvAlexander"
 project_name = "easy_readme"
 
@@ -18,11 +17,29 @@ project_urls = {
     "contrib-rocks-url": f"https://contrib.rocks/image?repo={username}/{project_name}"
 }
 
+def create_shields(**kwargs)->str:
+    options = { "contrib": "[![Contributors][contributors-shield]][contributors-url]", 
+                "fork": "[![Forks][forks-shield]][forks-url]",
+                "star": "[![Stargazers][stars-shield]][stars-url]", 
+                "issues": "[![Issues][issues-shield]][issues-url]", 
+                "license": "[![MIT License][license-shield]][license-url]", 
+                "linkedin": "[![LinkedIn][linkedin-shield]][linkedin-url]"}
+    shield_str = ""
+    for key, value in kwargs.items():
+        if key in options and value:
+            shield_str += f"{options[key]}\n"
+        elif key in options and not value:
+            print(f"{key} exists but pass")
+        else:
+            print(f"{key} does not exist")
+    return shield_str
+
 
 def create_readme():
     file_path = "./output/README.md"
     file = open(file_path, "w")
-    file.write("# Test")
+    shields = create_shields(contrib = False, fork = True, star = False, issues = True, license = True, linkedin = True, test = True, test_false = False)
+    file.write(f"{shields}")
 
 def get_repo():
     repo_path = "./"
@@ -33,5 +50,5 @@ def get_repo():
 
 
 if __name__ == "__main__":
-    # create_readme()
-    print(get_repo())
+    create_readme()
+    # print(get_repo())

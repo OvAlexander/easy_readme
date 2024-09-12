a4fc544992ac560f365caed2eaabd1527bd81b39
OvAlexander <alexanderleeov@gmail.com>
2024-09-12 09:10:02 -0700

    feat: created user data class

---
+++ user.py
@@ -0,0 +1,7 @@
+class User:
+    def __init__(self) -> None:
+        self.name = "John Doe"
+        self.username = "DoeJohn"
+        self.email = "JohnDoe@example.com"
+        self.linkedin = "John-Doe"
+        self.twitter = "John-Doe"

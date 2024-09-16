ad8d49526a605de49591dc11b3805d6a8a4598f8
OvAlexander <alexanderleeov@gmail.com>
2024-09-16 01:44:22 -0700

    feat: created basic gui for inputing markdown

---
+++ gui.py
@@ -0,0 +1,69 @@
+import customtkinter
+import markdown
+from markdown2 import Markdown
+from tkhtmlview import HTMLLabel
+
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 480
+APPEARANCE_MODE = ["system", "dark", "light"]
+
+class TextBoxFrame(customtkinter.CTkFrame):
+    def __init__(self, master, title):
+        super().__init__(master)
+        self.label = customtkinter.CTkLabel(self, text=title, fg_color="transparent", justify="center", anchor = "center")
+        self.label.grid(row=0, column=0, sticky="NSEW")
+        self.text_box = customtkinter.CTkTextbox(self)
+        self.text_box.grid(row=1, column=0, padx=10, pady=10, sticky="NSEW")
+        self.raw_markdown = "tEST"
+        self.html = "<h1>test</h1>"
+
+        self.tabview = customtkinter.CTkTabview(self)
+        self.tabview.add("raw")
+        self.tabview.add("markdown")
+        self.tabview.set("markdown")
+
+        self.raw_text_box = customtkinter.CTkTextbox(master=self.tabview.tab("raw"), fg_color="transparent", bg_color="white")
+        self.raw_text_box.insert("0.0", self.raw_markdown)
+        self.raw_text_box.grid(row=0, column=0)
+
+        self.html_label = HTMLLabel(master=self.tabview.tab("markdown"))
+        self.html_label.set_html(self.html)
+        self.html_label.grid(row=0, column=0)
+
+        self.tabview.grid(row=1,column=1, sticky="NSEW")
+
+        self.update_button = customtkinter.CTkButton(self, text="Update", command=self.update)
+        self.update_button.grid(row=2,column=0, columnspan=2, sticky="NSEW")
+        
+        
+    def update(self):
+        self.raw_markdown = self.text_box.get("0.0", "end")
+        self.html = Markdown().convert(self.raw_markdown)
+        self.raw_text_box.configure(state="normal")
+        self.raw_text_box.delete("0.0", "end")
+        self.raw_text_box.insert("0.0", self.raw_markdown)
+        self.raw_text_box.configure(state="disabled")
+        self.html_label.set_html(self.html)
+    
+
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("EASY README")
+        self.geometry(f'{SCREEN_WIDTH}x{SCREEN_HEIGHT}')
+        self.grid_columnconfigure((0,1), weight=1)
+        self.grid_rowconfigure((0,1), weight=1)
+        self._set_appearance_mode(APPEARANCE_MODE[1])
+
+        self.label = customtkinter.CTkLabel(self, text="EASY README", fg_color="transparent")
+        self.label.grid(row=0, column=0, sticky = "NSEW", columnspan=2)
+
+        self.ack_frame = TextBoxFrame(self, title="Acknowledgements")
+        self.ack_frame.grid(row=1,column=0,sticky="NSWE")
+
+        
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()

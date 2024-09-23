dd852712cd6d110f7e53b73ee4c7dd8b93d50fb4
OvAlexander <alexanderleeov@gmail.com>
2024-09-22 18:07:03 -0700

    feat: added tab view for all needed readme sections

--- gui.py
+++ gui.py
@@ -16,6 +16,8 @@ class TextBoxFrame(customtkinter.CTkFrame):
         self.text_box.grid(row=1, column=0, padx=10, pady=10, sticky="NSEW")
         self.raw_markdown = "tEST"
         self.html = "<h1>test</h1>"
+        self.title = title.lower().replace(" ", "_")
+        print(self.title)
 
         self.tabview = customtkinter.CTkTabview(self)
         self.tabview.add("raw")
@@ -33,7 +35,11 @@ class TextBoxFrame(customtkinter.CTkFrame):
         self.tabview.grid(row=1,column=1, sticky="NSEW")
 
         self.update_button = customtkinter.CTkButton(self, text="Update", command=self.update)
-        self.update_button.grid(row=2,column=0, columnspan=2, sticky="NSEW")
+        self.update_button.grid(row=2,column=0, columnspan=1, sticky="NSEW")
+
+        self.push_button = customtkinter.CTkButton(self, text="Push", command=self.push(self.title))
+        self.push_button.grid(row=2,column=1, columnspan=1, sticky="NSEW")
+        self.fetch_readme(self.title)
         
         
     def update(self):
@@ -45,6 +51,25 @@ class TextBoxFrame(customtkinter.CTkFrame):
         self.raw_text_box.configure(state="disabled")
         self.html_label.set_html(self.html)
     
+    def push(self, file_name):
+        self.output_text = self.text_box.get("0.0", "end")
+        file_path = f"./output/{file_name}.md"
+        file = open(file_path, "w")
+        print(f"Writing\n{self.output_text}")
+        file.write(self.output_text)
+        file.close()
+
+    def fetch_readme(self, file: str):
+        file = f"./templates/{file}.md"
+        md_file = open(file, "r")
+        md_text = md_file.readlines()
+        md_file.close()
+        parsed_text = ""
+        for text in md_text:
+            parsed_text += text
+        self.text_box.insert("0.0", parsed_text)
+        self.update()
+
 
 class App(customtkinter.CTk):
     def __init__(self):
@@ -59,8 +84,21 @@ class App(customtkinter.CTk):
         self.label = customtkinter.CTkLabel(self, text="EASY README", fg_color="transparent")
         self.label.grid(row=0, column=0, sticky = "NSEW", columnspan=2)
 
-        self.ack_frame = TextBoxFrame(self, title="Acknowledgements")
+        self.tabview = customtkinter.CTkTabview(self)
+        self.tabview.add("Acknowledgements")
+        self.tabview.add("Contributing")
+        self.tabview.add("Getting Started")
+        self.tabview.add("License")
+        self.tabview.set("Acknowledgements")
+        self.ack_frame = TextBoxFrame(self.tabview.tab("Acknowledgements"), title="Acknowledgements")
         self.ack_frame.grid(row=1,column=0,sticky="NSWE")
+        self.ack_frame = TextBoxFrame(self.tabview.tab("Contributing"), title="Contributing")
+        self.ack_frame.grid(row=1,column=1,sticky="NSWE")
+        self.ack_frame = TextBoxFrame(self.tabview.tab("Getting Started"), title="Getting Started")
+        self.ack_frame.grid(row=1,column=1,sticky="NSWE")
+        self.ack_frame = TextBoxFrame(self.tabview.tab("License"), title="License")
+        self.ack_frame.grid(row=1,column=1,sticky="NSWE")
+        self.tabview.grid(row=1,column=0, columnspan=2, sticky="NSEW")
 
         
 
